d280486857df7cc7b52beedc32f4f5efb0d7a4c4
George Ciesinski <george.ciesin@gmail.com>
2023-01-11 22:27:07 -0500

    Updated Content Background Color - Uses palette colors

--- src/copyright.js
+++ src/copyright.js
@@ -3,7 +3,7 @@ const copyright = function generateCopyright() {
     // Generates the innerHTML for copyright message at the bottom of the page
     const currentYear = new Date().getFullYear();
         // Link and font awesome icon
-        const githubLink = '<a href="https://github.com/GeorgeCiesinski"><i class="fa-brands github-link fa-github"></i></a>';
+        const githubLink = '<a href="https://github.com/GeorgeCiesinski"><i class="fa-brands github-link fa-github github-link"></i></a>';
         const copyrightInner = `Copyright &copy ${currentYear} George Ciesinski ${githubLink}`;
         return copyrightInner;
 };
